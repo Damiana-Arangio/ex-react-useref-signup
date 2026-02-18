e79e7bb85484e81dc4b7fe2d8c33b68bfc4b9fcc
Damiana-Arangio <arangio.damiana@gmail.com>
2026-02-18 11:51:34 +0100

    feat: completata milestone 2 con validazione in tempo reale

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import './App.css'
+
+// Set di caratteri validi per la validazione di username e password
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = `!@#$%^&*()-_=+[]{}|;:'\\",.<>?/\\\`~`;
+
 
 function App() {
 
@@ -16,6 +22,73 @@ function App() {
     anniEsperienza: "",
     descrizioneSviluppatore: ""
   });
+
+  /*****************
+      VALIDAZIONI
+  ******************/
+
+  // Validazione username
+  const isUsernameValid = useMemo(() => {
+
+    const username = formData.username.toLowerCase();
+    let isValid = true;
+
+    // Controllo lunghezza
+    if (username.length < 6) {
+      isValid = false;
+    } else {
+
+      // Controllo caratteri
+      const usernameArray = username.split("");
+      isValid = usernameArray.every((carattere) => (
+        letters.includes(carattere) || numbers.includes(carattere)
+      ));
+    }
+
+    return isValid;
+
+  }, [formData.username]);
+
+  // Validazione password
+  const isPasswordValid = useMemo( () => {
+    const password = formData.password.toLowerCase();
+    let isValid = true;
+
+    // Controllo lunghezza
+    if(password.length <8 ){
+      isValid = false;
+    }
+    else {
+      // Controllo caratteri
+      const passwordArray = password.split("");
+
+      // Controllo se ha almeno una lettera
+      const hasLetters = passwordArray.some( (carattere) => (letters.includes(carattere)))
+
+      // Controllo se ha almeno un numero
+      const hasNumbers = passwordArray.some((carattere) => (numbers.includes(carattere)))
+
+      // Controllo se ha almeno un simbolo
+      const hasSymbols = passwordArray.some((carattere) => (symbols.includes(carattere)))
+
+      isValid = hasLetters && hasNumbers && hasSymbols;
+    }
+
+    return isValid;
+
+  }, [formData.password])
+
+  // Validazione descrizione
+  const isDescriptionValid = useMemo( () => {
+    const description = formData.descrizioneSviluppatore.trim();
+    let isValid = true;
+
+    if ( !(description.length >= 100 && description.length <= 1000))
+      isValid = false;
+
+    return isValid;
+
+  }, [formData.descrizioneSviluppatore]);
 
 
   /************
@@ -53,6 +126,17 @@ function App() {
             value={formData.username}
             onChange={handleFormData}
           />
+
+          {/* Validazione */}
+          {
+            formData.username && (
+              <p className={isUsernameValid ? "success" : "error"}> 
+              { isUsernameValid  
+                ? "Username valido!" 
+                : "Deve contenere almeno 6 caratteri alfanumerici!"} 
+            </p>
+            )
+          }
         </div>
 
         {/* Password */}
@@ -65,6 +149,17 @@ function App() {
             value={formData.password}
             onChange={handleFormData}
           />
+
+          {/* Validazione */}
+          {
+            formData.password && (
+              <p className={isPasswordValid ? "success" : "error"}>
+                {isPasswordValid
+                  ? "Password valida!"
+                  : "Deve contenere almeno 8 caratteri, 1 lettera, 1 numero e 1 simbolo!"}
+              </p>
+            )
+          }
         </div>
 
         {/* Specializzazione */}
@@ -107,6 +202,17 @@ function App() {
             value={formData.descrizioneSviluppatore}
             onChange={handleFormData}
           />
+
+          {/* Validazione */}
+          {
+            formData.descrizioneSviluppatore && (
+              <p className={isDescriptionValid ? "success" : "error"}>
+                {isDescriptionValid
+                  ? "Descrizione valida!"
+                  : `Deve contenere tra 100 e 1000 caratteri! (${formData.descrizioneSviluppatore.trim().length}/1000)`}
+              </p>
+            )
+          }
         </div>
 
         <button type="submit">Registrati</button>
@@ -126,9 +232,7 @@ function App() {
     const { fullName, username, password, specializzazione, anniEsperienza, descrizioneSviluppatore } = formData;
 
     // Controllo campi vuoti
-    if (
-      !fullName || !username || !password || !specializzazione || !anniEsperienza || !descrizioneSviluppatore
-    ) {
+    if ( !fullName || !username || !password || !specializzazione || !anniEsperienza || !descrizioneSviluppatore) {
       alert("Tutti i campi sono obbligatori");
       return;
     }
@@ -136,6 +240,12 @@ function App() {
     // Controllo numero positivo
     if (Number(anniEsperienza) <= 0) {
       alert("Gli anni di esperienza devono essere un numero positivo");
+      return;
+    }
+
+    // Controllo validazioni
+    if (!isUsernameValid || !isPasswordValid || !isDescriptionValid) {
+      alert("Alcuni campi non sono validi!");
       return;
     }
 
